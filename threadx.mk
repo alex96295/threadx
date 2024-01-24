d6a1149e6bf14a6bbeddfa71e443b1321323f5ed
aottaviano <aottaviano@iis.ee.ethz.ch>
2024-01-24 11:47:03 +0100

    threadx.mk: Add build targets

--- threadx.mk
+++ threadx.mk
@@ -3,9 +3,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Author: Alessandro Ottaviano <aottaviano@iis.ee.ethz.ch>
+
+# This makefrag is meant to be included in the Makefile or other makefrag
+# managing the SW build of the target PULP system.
+
 ###########
 # Threadx #
 ###########
+
+CMAKE ?= cmake-3.18.1
+TGT   ?= safety_island
 
 TX_DIR ?= .
 TX_SW_LIBS = $(TX_DIR)/build/libthreadx.a
@@ -13,3 +21,14 @@ TX_INCS := -I$(TX_DIR)/common/inc
 TX_INCS += -I$(TX_DIR)/common_modules/inc
 TX_INCS += -I$(TX_DIR)/common_smp/inc
 TX_INCS += -I$(TX_DIR)/ports
+
+$(TX_DIR)/build/libthreadx.a:
+	cd $(TX_DIR); $(CMAKE) -Bbuild -GNinja -DCMAKE_TOOLCHAIN_FILE=$(TX_DIR)/cmake/$(TGT).cmake .
+	cd $(TX_DIR); $(CMAKE) --build $(TX_DIR)/build
+
+.PHONY: tx-lib-build
+tx-lib-build: $(TX_DIR)/build/libthreadx.a
+
+.PHONY: tx-lib-clean
+tx-lib-clean:
+	rm -rf $(TX_DIR)/build
