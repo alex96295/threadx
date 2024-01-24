c60f05aa9fd48abcc74d8fe5e4e9b4ee51041265
aottaviano <aottaviano@iis.ee.ethz.ch>
2024-01-24 11:46:31 +0100

    cmake: Remove platform-specific define
    * Useless here since code is platform-agnostic

--- cmake/cheshire.cmake
+++ cmake/cheshire.cmake
@@ -5,7 +5,7 @@ set(CMAKE_SYSTEM_PROCESSOR cheshire)
 set(THREADX_ARCH "cheshire")
 set(THREADX_TOOLCHAIN "gnu")
 
-set(MCPU_FLAGS "-march=rv64gc_zifencei -mabi=lp64d -DOT_PLATFORM_RV32 -frandom-seed=cheshire -fuse-linker-plugin -flto")
+set(MCPU_FLAGS "-march=rv64gc_zifencei -mabi=lp64d -frandom-seed=cheshire -fuse-linker-plugin -flto")
 set(VFP_FLAGS "")
 set(SPEC_FLAGS "")
 # set(LD_FLAGS "-nostartfiles -Wl,--gc-sections -Wl,-flto")
